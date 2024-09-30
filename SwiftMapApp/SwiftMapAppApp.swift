53ba4daba77a1ef7bb69f54919e567c4de7212c2
Talha Mirza <mirzatalha178@gmail.com>
2024-09-30 14:22:10 +0500

    Initial Commit

---
+++ SwiftMapApp/SwiftMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapAppApp.swift
+//  SwiftMapApp
+//
+//  Created by Tayyab  on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
